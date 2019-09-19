dca5a8b114c544dc8f415dc41f91ca85f92eb5a3
Dhairyasharma504 <you@example.com>
2019-09-19 13:51:23 +0530

    minor changes

--- src/components/Service.js
+++ src/components/Service.js
@@ -18,53 +18,52 @@ const Wrapper = styled.div`
 const service = () => (
   <Wrapper>
     <div className="main">
-      <body>
-        <section className="section">
-          <div className="container">
-            <div className="columns">
-              <div className="column">
-                <div className="card">
-                  <div className="card-content">
-                    <h1 className="title is-4 is-spaced has-text-center">
-                      Primary Care
-                    </h1>
-                    <h5 className="subtitle is-6 has-text-weight-normal is-family-sans-serif has-text-center is-spaced ">
-                      An so regular to on points wanted rapture ous resolving
-                      continued household
-                    </h5>
-                  </div>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <i className="fas fa-heartbeat" />
+                  <h1 className="title is-4 is-spaced has-text-center">
+                    Primary Care
+                  </h1>
+                  <h5 className="subtitle is-6 has-text-weight-normal is-family-sans-serif has-text-center is-spaced ">
+                    An so regular to on points wanted rapture ous resolving
+                    continued household
+                  </h5>
                 </div>
               </div>
-              <div className="column">
-                <div className="card">
-                  <div className="card-content">
-                    <h1 className="title is-4 is-spaced has-text-center">
-                      Emergency Cases
-                    </h1>
-                    <h5 className="subtitle is-6 has-text-weight-normal is-family-sans-serif has-text-center is-spaced">
-                      An so regular to on points wanted rapture ous resolving
-                      continued household
-                    </h5>
-                  </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <h1 className="title is-4 is-spaced has-text-center">
+                    Emergency Cases
+                  </h1>
+                  <h5 className="subtitle is-6 has-text-weight-normal is-family-sans-serif has-text-center is-spaced">
+                    An so regular to on points wanted rapture ous resolving
+                    continued household
+                  </h5>
                 </div>
               </div>
-              <div className="column">
-                <div className="card">
-                  <div className="card-content">
-                    <h1 className="title is-4 is-spaced has-text-center">
-                      Online Appointment
-                    </h1>
-                    <h5 className="subtitle is-6 has-text-weight-normal is-family-sans-serif has-text-center is-spaced">
-                      An so regular to on points wanted rapture ous resolving
-                      continued household
-                    </h5>
-                  </div>
+            </div>
+            <div className="column">
+              <div className="card">
+                <div className="card-content">
+                  <h1 className="title is-4 is-spaced has-text-center">
+                    Online Appointment
+                  </h1>
+                  <h5 className="subtitle is-6 has-text-weight-normal is-family-sans-serif has-text-center is-spaced">
+                    An so regular to on points wanted rapture ous resolving
+                    continued household
+                  </h5>
                 </div>
               </div>
             </div>
           </div>
-        </section>
-      </body>
+        </div>
+      </section>
     </div>
   </Wrapper>
 );
--- src/components/Text2.js
+++ src/components/Text2.js
@@ -14,68 +14,79 @@ const Wrapper = styled.div`
 `;
 const Text2 = () => (
   <Wrapper>
-    <section className="hero">
-      <div className="hero-body">
-        <div className="container">
-          <div className="columns">
-            <div className="column">
-              <h1 className="title  is-1 has-text-left is-spaced has-text-grey-dark has-text-centered">
-                Medcare Experience Doctors
-              </h1>
+    <div className="hero-body">
+      <div className="container">
+        <div className="columns">
+          <div className="column">
+            <h1 className="title  is-1 has-text-left is-spaced has-text-grey-dark has-text-centered has-text-centered">
+              Medcare Experience Doctors
+            </h1>
+          </div>
+        </div>
+        <div className="columns">
+          <div className="column">
+            <div className="card">
+              <figure className="image">
+                <img
+                  src="https://colorlib.com/preview/theme/medcare/img/team/1.jpg"
+                  alt="eee "
+                />
+              </figure>
+              <div className="container">
+                <section className="section">
+                  <h1 className="title is-4 has-text-centered">
+                    Dr Adam Brain
+                  </h1>
+                  <h2 className="subtitle  is-6 has-text-weight-normal is-family-sans-serif has-text-centered">
+                    Subtitle
+                  </h2>
+                </section>
+              </div>
             </div>
           </div>
-          <section className="section">
-            <div className="columns">
-              <div className="column">
-                <div className="card">
-                  <figure className="image">
-                    <img src="https://colorlib.com/preview/theme/medcare/img/team/1.jpg" />
-                  </figure>
-                  <div className="container">
-                    <section className="section">
-                      <h1 className="title is-4 has-text-centered">
-                        Dr Adam Brain
-                      </h1>
-                      <h2 className="subtitle">Subtitle</h2>
-                    </section>
-                  </div>
-                </div>
-              </div>
-              <div className="column">
-                <div className="card">
-                  <figure className="image">
-                    <img src="https://colorlib.com/preview/theme/medcare/img/team/2.jpg" />
-                  </figure>
-                  <div className="container">
-                    <section className="section">
-                      <h1 className="title is-4 has-text-centered">
-                        Dr Blian Judge
-                      </h1>
-                      <h2 className="subtitle">Subtitle</h2>
-                    </section>
-                  </div>
-                </div>
-              </div>
-              <div className="column">
-                <div className="card">
-                  <figure className="image">
-                    <img src="https://colorlib.com/preview/theme/medcare/img/team/3.jpg" />
-                  </figure>
-                  <div className="container">
-                    <section className="section">
-                      <h1 className="title is-4 has-text-centered">
-                        Dr Blian Judge
-                      </h1>
-                      <h2 className="subtitle">Subtitle</h2>
-                    </section>
-                  </div>
-                </div>
+          <div className="column">
+            <div className="card">
+              <figure className="image">
+                <img
+                  src="https://colorlib.com/preview/theme/medcare/img/team/2.jpg"
+                  alt="rrr"
+                />
+              </figure>
+              <div className="container">
+                <section className="section">
+                  <h1 className="title is-4 has-text-centered">
+                    Dr Blian Judge
+                  </h1>
+                  <h2 className="subtitle has-text-centered is-6 has-text-weight-normal is-family-sans-serif">
+                    Subtitle
+                  </h2>
+                </section>
               </div>
             </div>
-          </section>
+          </div>
+          <div className="column">
+            <div className="card">
+              <figure className="image">
+                <img
+                  src="https://colorlib.com/preview/theme/medcare/img/team/3.jpg"
+                  alt="place"
+                />
+              </figure>
+              <div className="container">
+                <section className="section">
+                  <h1 className="title is-4 has-text-centered">
+                    Dr Blian Judge
+                  </h1>
+                  <h2 className="subtitle has-text-centered is-6 has-text-weight-normal is-family-sans-serif">
+                    Subtitle
+                  </h2>
+                </section>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </section>
+    </div>
   </Wrapper>
 );
 export default Text2;
